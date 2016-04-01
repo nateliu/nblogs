e778217da14f03dba0732bb685338f815ddc2420
nateliu <jinliangliu@163.com>
2016-04-01 20:04:26 +0800

    add bower to maintenance bootstrap/add eclipse project file

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use(function (err, req, res, next) {
   var meta = '[' + new Date() + '] ' + req.url + '\n';
