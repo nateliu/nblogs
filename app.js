2b8bb68e1a3ccacebba1b035727ef27fe82abc22
nate <jinliangliu@163.com>
2016-04-24 20:34:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//this is a test
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
